4303b304368e921ad868b767657536e5492fe90e
Bianca Rosa <bianca@lumosidentity.com>
2022-04-09 14:37:42 -0300

    add task status columns

--- app/tasks/models.py
+++ app/tasks/models.py
@@ -1,11 +1,20 @@
 """app.tasks.models
 
 Module that deals with Tasks models."""
+import enum
+
 from app.main import db
+
+class TaskStatus(enum.Enum):
+    TO_DO = 1
+    DOING = 2
+    DONE = 3
+
 class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, unique=True)
     description = db.Column(db.String)
+    status = db.Column(db.Enum(TaskStatus), default=TaskStatus.TO_DO)
 
     def __init__(self, description):
         self.description = description
