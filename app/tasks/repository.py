4acf1c5636d2a7c2d3e66a57877de40839280782
Bianca Rosa <bianca@lumosidentity.com>
2022-04-21 13:30:57 -0300

    add extra methods to repository

--- app/tasks/repository.py
+++ app/tasks/repository.py
@@ -12,9 +12,17 @@ class TaskRepository:
         db.session.commit()
         return task
     
+    def bulk_save(self, tasks):
+        for task in tasks: 
+            db.session.add(task)
+        db.session.commit()
+        return tasks
+    
     def get(self, id):
         return db.session.query(models.Task).get(id)
-
     
     def list(self):
         return models.Task.query.order_by(models.Task.id.asc()).limit(self.DEFAULT_LIMIT).all()
+
+    def get_many(self, ids):
+        return models.Task.query.filter(models.Task.id.in_(ids)).all()
