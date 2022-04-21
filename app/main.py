dffd0a7247a2428a3381b4359d55e53c03b836c2
Bianca Rosa <bianca@lumosidentity.com>
2022-04-21 13:30:48 -0300

    always init app

--- app/main.py
+++ app/main.py
@@ -18,22 +18,23 @@ def populate_db():
     for i in range(0,15000):
         task = models.Task(f"Task {i}")
         tasks_repository.save(task)
-        
-init_logger()
-logger = logging.getLogger(__name__)
 
 # pylint: disable=C0103
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:////tmp/test.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 from app.healthcheck import blueprint as health_check_blueprint
 from app.tasks import blueprint as tasks_blueprint
 
 app.register_blueprint(health_check_blueprint.create_blueprint())
 app.register_blueprint(tasks_blueprint.create_blueprint())
 
+newrelic.agent.initialize()
+newrelic.agent.register_application()
+        
+init_logger()
+logger = logging.getLogger(__name__)
 
 try:
     logger.info('Creating db schema.')
